2811a1c459a0ed434886d23c92fa1d00a06d96cc
Frédéric Lachapelle <frederic.lachapelle7@gmail.com>
2024-10-21 23:38:28 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/open_tv/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.open_tv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/bottom_nav.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatefulWidget {
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  int _selectedIndex = 0;
+
+  void onBarTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'All',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: onBarTapped,
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+}
