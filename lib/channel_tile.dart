2f3176edc0f14dc0f42d21e25587c62d56ea7a81
Frédéric Lachapelle <frederic.lachapelle7@gmail.com>
2025-02-10 01:21:39 -0700

    fix ellipsis

--- lib/channel_tile.dart
+++ lib/channel_tile.dart
@@ -33,34 +33,42 @@ class _ChannelTileState extends State<ChannelTile> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: _focusNode.hasFocus ? 8.0 : 4.0, // Highlight when focused
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      color: _focusNode.hasFocus ? Colors.blue.shade100 : Colors.white,
-      child: InkWell(
+        elevation: _focusNode.hasFocus ? 8.0 : 4.0, // Highlight when focused
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        color: _focusNode.hasFocus ? Colors.blue.shade100 : Colors.white,
+        child: InkWell(
           focusNode: _focusNode,
           onTap: () => print("Selected ${widget.channel.name}"),
           borderRadius: BorderRadius.circular(10),
-          child: Padding(padding: const EdgeInsets.all(10), child: Row(
-            children: [
-              Expanded(
-                  flex: 3,
-                  child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: widget.channel.image != null
-                          ? CachedNetworkImage(
-                              fit: BoxFit.contain,
-                              imageUrl: widget.channel.image!,
-                            )
-                          : Image.asset(
-                              "assets/icon.png",
-                              fit: BoxFit.contain,
-                            ))),
-              const Expanded(flex: 1, child: SizedBox()),
-              Expanded(flex: 8, child: Text(widget.channel.name))
-            ],
-          )),
-    ));
+          child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Expanded(
+                      flex: 3,
+                      child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: widget.channel.image != null
+                              ? CachedNetworkImage(
+                                  fit: BoxFit.contain,
+                                  imageUrl: widget.channel.image!,
+                                )
+                              : Image.asset(
+                                  "assets/icon.png",
+                                  fit: BoxFit.contain,
+                                ))),
+                  const Expanded(flex: 1, child: SizedBox()),
+                  Expanded(
+                      flex: 8,
+                      child: Text(
+                        widget.channel.name,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                      ))
+                ],
+              )),
+        ));
   }
 }
