435baf4612970ddac810e1fdf3e6bc1bb0985ed3
Frédéric Lachapelle <frederic.lachapelle7@gmail.com>
2025-07-05 00:24:32 -0400

    improve player

--- lib/player.dart
+++ lib/player.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:media_kit_video/media_kit_video.dart';
 import 'package:open_tv/backend/sql.dart';
+import 'package:open_tv/home.dart';
 import 'package:open_tv/models/channel.dart';
 import 'package:open_tv/models/media_type.dart';
 import 'package:media_kit/media_kit.dart' as mk;
@@ -30,25 +31,15 @@ class _PlayerState extends State<Player> {
   }
 
   Future<void> initAsync() async {
-    await player.open(mk.Media(widget.channel.url!));
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
-      await key.currentState?.enterFullscreen();
-    });
+    final seconds = widget.channel.mediaType == MediaType.movie
+        ? await Sql.getPosition(widget.channel.id!)
+        : null;
+    await player.open(mk.Media(
+      widget.channel.url!,
+      start: seconds != null ? Duration(seconds: seconds) : null,
+    ));
+    await key.currentState?.enterFullscreen();
     player.setPlaylistMode(mk.PlaylistMode.single);
-    if (widget.channel.mediaType == MediaType.movie) {
-      setLastPosition();
-    }
-  }
-
-  Future<void> setLastPosition() async {
-    var seconds = await Sql.getPosition(widget.channel.id!);
-    if (seconds != null) {
-      subscription = player.stream.duration.listen((event) {
-        if (event.inSeconds == 0) return;
-        player.seek(Duration(seconds: seconds));
-        subscription.cancel();
-      });
-    }
   }
 
   @override
@@ -62,31 +53,49 @@ class _PlayerState extends State<Player> {
     return Scaffold(
         backgroundColor: Colors.black,
         body: MaterialVideoControlsTheme(
-          normal: const MaterialVideoControlsThemeData(),
-          fullscreen: MaterialVideoControlsThemeData(
-              speedUpOnLongPress: false,
-              seekOnDoubleTap: widget.channel.mediaType != MediaType.livestream,
-              displaySeekBar: widget.channel.mediaType != MediaType.livestream,
-              seekBarMargin: const EdgeInsets.only(bottom: 60),
-              seekGesture: widget.channel.mediaType != MediaType.livestream),
+          normal: getThemeData(context),
+          fullscreen: getThemeData(context),
           child: Video(
-              key: key,
-              controller: videoController,
-              onExitFullscreen: () async {
-                if (widget.channel.mediaType == MediaType.movie) {
-                  Sql.setPosition(
-                      widget.channel.id!, player.state.position.inSeconds);
-                }
-                Navigator.of(context).pop();
-                Navigator.of(context).pop();
-                SystemChrome.setPreferredOrientations([
-                  DeviceOrientation.portraitUp,
-                  DeviceOrientation.portraitDown,
-                  DeviceOrientation.landscapeLeft,
-                  DeviceOrientation.landscapeRight,
-                ]);
-                SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-              }),
+            key: key,
+            controller: videoController,
+            onExitFullscreen: () async => onExit(),
+          ),
         ));
+  }
+
+  void onExit() async {
+    if (widget.channel.mediaType == MediaType.movie) {
+      Sql.setPosition(widget.channel.id!, player.state.position.inSeconds);
+    }
+    await key.currentState!.exitFullscreen();
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => Home()));
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  }
+
+  MaterialVideoControlsThemeData getThemeData(BuildContext context) {
+    return MaterialVideoControlsThemeData(
+      speedUpOnLongPress: false,
+      seekOnDoubleTap: widget.channel.mediaType != MediaType.livestream,
+      displaySeekBar: widget.channel.mediaType != MediaType.livestream,
+      seekBarMargin: const EdgeInsets.only(bottom: 60),
+      seekBarThumbSize: 20,
+      seekBarHeight: 10,
+      seekGesture: widget.channel.mediaType != MediaType.livestream,
+      topButtonBar: [
+        IconButton(
+          onPressed: onExit,
+          icon: const Icon(Icons.arrow_back, color: Colors.white, size: 32),
+        ),
+        const SizedBox(width: 10),
+        Text(widget.channel.name),
+      ],
+    );
   }
 }
