48ff8ea1c415de3427151ff9abfb44fd0bd928f9
Frédéric Lachapelle <frederic.lachapelle7@gmail.com>
2026-01-15 00:01:51 -0500

    bump version

--- lib/whats_new_modal.dart
+++ lib/whats_new_modal.dart
@@ -9,43 +9,45 @@ class WhatsNewModal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-        title: Text("What's new: update $version"),
-        actions: [
-          TextButton(
-              onPressed: () async {
-                await launchUrl(
-                    Uri.parse(
-                      "https://github.com/Fredolx/fred-tv-mobile/discussions/1",
-                    ),
-                    mode: LaunchMode.externalApplication);
-                Navigator.pop(context, false);
-              },
-              child: const Text("Donate")),
-          TextButton(
-              onPressed: () async {
-                await SettingsService.updateLastSeenVersion();
-                Navigator.pop(context, true);
-              },
-              child: const Text("Don't show again"))
-        ],
-        content: Scrollbar(
-          thumbVisibility: true,
-          child: SingleChildScrollView(
-              child: const Padding(
-                  padding: EdgeInsets.only(right: 8.0),
-                  child: const Text(
-                    '''
+      title: Text("What's new: update $version"),
+      actions: [
+        TextButton(
+          onPressed: () async {
+            await launchUrl(
+              Uri.parse(
+                "https://github.com/Fredolx/fred-tv-mobile/discussions/1",
+              ),
+              mode: LaunchMode.externalApplication,
+            );
+            Navigator.pop(context, false);
+          },
+          child: const Text("Donate"),
+        ),
+        TextButton(
+          onPressed: () async {
+            await SettingsService.updateLastSeenVersion();
+            Navigator.pop(context, true);
+          },
+          child: const Text("Don't show again"),
+        ),
+      ],
+      content: Scrollbar(
+        thumbVisibility: true,
+        child: SingleChildScrollView(
+          child: const Padding(
+            padding: EdgeInsets.only(right: 8.0),
+            child: const Text('''
 Hi! Thanks for supporting Fred TV. Here's everything new:
 
-- Improved playback. It should not loop anymore on livestreams
 - Improved UI
-- Remade the setup experience
-- Added Android TV support
-- More to come!
+- Fixed Xtream importing bugs
+- Improved Android TV support
 
 If you like Fred TV, please consider donating (Even a dollar helps tremendously), reporting issues on the github and sharing it with your friends.
-''',
-                  ))),
-        ));
+'''),
+          ),
+        ),
+      ),
+    );
   }
 }
